6a441d8fec4c3463363d9ac7ce85600835a7da38
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-03-25 22:42:23 +0100

    comments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  transpilePackages: ['next-mdx-remote'],
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Ignore ESLint warnings in production
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ✅ Ignore TypeScript errors in production
+  },
+  // Optionally, add any other Next.js config below
+}
 
 export default nextConfig;
